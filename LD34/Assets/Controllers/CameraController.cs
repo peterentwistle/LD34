20f1965db503158e0b7af7c75d4f16b0e0e724c6
Peter Entwistle <petertheanimator@gmail.com>
2015-12-12 15:47:24 +0000

    Added simple camera movement

---
+++ LD34/Assets/Controllers/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	
+	private Vector3	dragStartPosition;
+	public float dragSpeed;
+
+	void Start() {
+	}
+
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			dragStartPosition = Input.mousePosition;
+			return;
+		}
+
+		if (!Input.GetMouseButton(0)) {
+			return;
+		}
+
+		Vector3 position = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragStartPosition);
+		Vector3 moveTo = new Vector3(position.x * dragSpeed, position.y * dragSpeed, 0);
+		transform.Translate(moveTo, Space.World);
+	}
+}
