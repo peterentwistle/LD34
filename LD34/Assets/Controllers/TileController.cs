82b5746dbc222865ce4415778d5f6346129766a0
Peter Entwistle <petertheanimator@gmail.com>
2015-12-12 18:41:30 +0000

    Added ability to see which tile the mouse is over

---
+++ LD34/Assets/Controllers/TileController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileController : MonoBehaviour {
+
+	void OnMouseOver() {
+		Debug.Log("Hit object: " + this.name);
+	}
+}
--- LD34/Assets/Controllers/WorldController.cs
+++ LD34/Assets/Controllers/WorldController.cs
@@ -1,32 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class WorldController : MonoBehaviour {
 
 	private World world;
-	public Sprite defaultTile;
+	public GameObject GroundTile;
+	public Sprite DefaultTile;
 
 	// Use this for initialization
 	void Start() {
-		world = new World(100, 100);
+		world = new World();
 
 		for (int x = 0; x < world.Width; x++) {
 			for (int y = 0; y < world.Height; y++) {
 				Tile tileDate = world.GetTileAt(x, y);
 					
-				GameObject tileGameObj = new GameObject();
+				GameObject tileGameObj = Instantiate(GroundTile);
 				tileGameObj.name = "Tile_X_"+x+"_Y_"+y;
 				tileGameObj.transform.position = new Vector3(tileDate.X, tileDate.Y, 0);
 
 				SpriteRenderer tileSpriteRenderer = tileGameObj.AddComponent<SpriteRenderer>();
+				tileGameObj.AddComponent<BoxCollider2D>();
 
-				tileSpriteRenderer.sprite = defaultTile;
+				tileSpriteRenderer.sprite = DefaultTile;
 			}
 		}
 	}
-	
+
 	// Update is called once per frame
 	void Update() {
-	
+		
 	}
 }
